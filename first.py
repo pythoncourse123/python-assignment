7201d3d0ff85f7c7e9a1e20b805020b488a135a2
Md Tarikul Islam <146817364+pythoncourse123@users.noreply.github.com>
2024-04-09 09:57:23 +0900

    Add files via upload

---
+++ first.py
@@ -0,0 +1,5 @@
+char=input('enter a single character:')
+if (char=='a'or char=='A'or char=='e'or char=='i'or char=='I'or char=='o'or char=='O'or char=='u'or char=='U'):
+    print(' char is vowel')
+else:
+    print('char is consonent')
